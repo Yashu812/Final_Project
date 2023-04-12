64a546d5459a7f73fc4a19edb5884e150a19dbd5
Yashashree Deshmukh <79332515+Yashu812@users.noreply.github.com>
2023-04-12 08:47:53 +0530

    Update app.py
    Modified upload function

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import json
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = 'static/images/uploads/'
+UPLOAD_FOLDER = 'static/images/uploads'
 
 app.secret_key = "yashashree"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -26,31 +26,25 @@ def home():
 def women():
     return render_template('women.html')
 
-@app.route('/upload', methods=['POST', 'GET'])
-def upload_image():
-    if 'file1' not in request.files or 'file3' not in request.files:
-        flash('No file part', 'missing')
-        return redirect(request.url)
-    file1 = request.files['file1']
-    file3 = request.files['file3']
+@app.route('/upload', methods=['POST'])
+def upload():
+    # Check if the request contains files
+    if 'image1' not in request.files or 'image2' not in request.files:
+        return 'No file(s) selected'
 
-    if file1.filename == '' or file3.filename == '':
-        flash('No image selected for uploading', 'empty')
-        return redirect(request.url)
+    # Get the files from the request
+    image1 = request.files['image1']
+    image2 = request.files['image2']
 
-    if file1 and allowed_file(file1.filename):
-        filename1 = secure_filename(file1.filename)
-        file1.save(os.path.join(app.config['UPLOAD_FOLDER'], filename1))
-        # print('upload_image filename: ' + filename)
-        # flash('Image successfully uploaded','success')
-    if file3 and allowed_file(file3.filename):
-        filename3 = secure_filename(file3.filename)
-        file3.save(os.path.join(app.config['UPLOAD_FOLDER'], filename3))
-        flash('Image successfully uploaded', 'success')
-        return redirect(url_for('women'))
-    else:
-        flash('Allowed image types are - png, jpg, jpeg')
-        return redirect(request.url)
+    # Save the images to the UPLOAD_FOLDER
+    filename1 = secure_filename(image1.filename)
+    filename2 = secure_filename(image2.filename)
+    image1.save(os.path.join(app.config['UPLOAD_FOLDER'], filename1))
+    image2.save(os.path.join(app.config['UPLOAD_FOLDER'], filename2))
+
+    # Return a success message
+    return 'Images uploaded successfully'
+
 
 @app.route('/guidelines')
 def guidelines():
@@ -60,6 +54,5 @@ def guidelines():
 def contact():
     return render_template('contact.html')
 
-
 if __name__ == "__main__":
-    app.run(port=8000, debug=True)
+    app.run(debug=True)
